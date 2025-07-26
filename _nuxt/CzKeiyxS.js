72e4c466c7a0f64a836241533ace2fcc6b797520
annebrown <anne@browndomain.com>
2025-07-26 09:18:49 +0000

    Deploying to gh-pages from @ annebrown/docs@c6b9c5f2e7d131d92ec6e6d5c9de651583af924c 🚀

---
+++ _nuxt/CzKeiyxS.js
@@ -0,0 +1,1 @@
+import{r as n}from"./tzFwfC_d.js";import a from"./gydX8Cpm.js";import{z as s,H as t,i as e,w as i,W as p}from"./Cvo98E39.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
